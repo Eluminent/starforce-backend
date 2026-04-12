d3b406a97f993445a49c789ef24c79d0bf3070c8
Chris <76500048+Eluminent@users.noreply.github.com>
2026-04-11 21:36:59 -0400

    Rate limiting function and moved register and login into functions.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
@@ -11,25 +12,47 @@ app.use(express.json());
 // In-memory user storage (for demonstration purposes only)
 const users = [];
 
-// REGISTER
-app.post("/register", async (req, res) => {
-  const { username, password } = req.body;
+
+
+//Limit number of user requests
+const limiter = rateLimit({
+  windowMs: 1000*60, //1 min
+  max: 10, //Limit IP to 10 requests
+  message: "Too many requests, please try again later."
+});
+
+
+
+// Register
+async function register(request, result) {
+  const {username, password} = request.body;
+
+  if (!username || !password) {
+    return result.status(400).send("Username and password required.");
+  }
+
+  const existingUser = users.find((u) => u.username === username);
+  if (existingUser) {
+    return result.status(400).send("User exists already!");
+  }
 
   const hashedPassword = await bcrypt.hash(password, 10);
 
   users.push({
     username,
-    password: hashedPassword
-  });
+    password: hashedPassword,
+  })
 
-  res.json({ message: "User registered" });
-});
+  result.json({message: "User registered!"});
+}
 
-// LOGIN
-app.post("/login", async (req, res) => {
+
+
+//login
+async function login(req, res) {
   const { username, password } = req.body;
 
-  const user = users.find(u => u.username === username);
+  const user = users.find((u) => u.username === username);
   if (!user) {
     return res.status(400).send("User not found");
   }
@@ -39,9 +62,22 @@ app.post("/login", async (req, res) => {
     return res.status(401).send("Invalid password");
   }
 
-  const token = jwt.sign({ username }, "secretkey");
+  const token = jwt.sign({ username }, process.env.JWT_SECRET, {
+    expiresIn: "1h",
+  });
 
   res.json({ token });
+}
+
+
+app.post("/register", register);
+app.post("/login", limiter, login);
+
+app.get("/profile", authMiddleware, (request, response) => {
+  response.json({
+    message: "You have been authenticated!",
+    user: request.user
+  });
 });
 
 app.get("/", (req, res) => {
